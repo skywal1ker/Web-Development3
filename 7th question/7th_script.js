d151473deed7be527999993dd73c179d9ce11c3c
skywal1ker <57444170+skywal1ker@users.noreply.github.com>
2022-04-23 04:28:18 -0400

    Add files via upload

---
+++ 7th question/7th_script.js
@@ -0,0 +1,4 @@
+const cars = ["Saab", "Volvo", "BMW", "Toyota", "Mercedes", "Mazda", "Chevrolet", "Ford", "Subaru", "Honda"]
+let x = cars[2]
+console.log(x)
+alert(cars)
---
+++ 9th question/script9.js
@@ -0,0 +1,5 @@
+var j = 0;
+const ball8 = "grilled cheese";
+for (let i = 1; i < j; i++) {
+    alert(`I d like to have tomatoes with my ${ball8} and a slice of deli ham on the side, please...`);
+}
